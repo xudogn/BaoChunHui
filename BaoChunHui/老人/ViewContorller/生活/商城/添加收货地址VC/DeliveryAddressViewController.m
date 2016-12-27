a6d78404f53540b9501e528ffaf2f1ef15569c03
xudogn <xudogn@qq.com>
2016-12-27 15:21:03 +0800

    添加收货地址button

--- BaoChunHui/老人/ViewContorller/生活/商城/添加收货地址VC/DeliveryAddressViewController.m
+++ BaoChunHui/老人/ViewContorller/生活/商城/添加收货地址VC/DeliveryAddressViewController.m
@@ -16,6 +16,7 @@
 
 @property(nonatomic, strong) NSMutableArray<addressModel *> *address_arr;
 
+@property(nonatomic, strong) UIButton *addAddress;
 
 
 @end
@@ -97,14 +98,35 @@
 
 
 
+- (void)addNewAddress{
+    // 跳转到添加新地址页面
+}
 
-
-
+- (UIButton *)addAddress{
+    if (!_addAddress) {
+        _addAddress = [UIButton buttonWithType:UIButtonTypeSystem];
+        [self.view addSubview:_addAddress];
+        [_addAddress mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.right.bottom.equalTo(0);
+            make.top.equalTo(self.tableView.mas_bottom);
+            make.height.equalTo(49);
+        }];
+        [_addAddress setTitle:@"添加新地址" forState:UIControlStateNormal];
+        [_addAddress setBackgroundColor:[UIColor ShengHuoColor]];
+        _addAddress.titleLabel.font = [UIFont systemFontOfSize:25];
+        [_addAddress addTarget:self action:@selector(addNewAddress) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _addAddress;
+}
 
 
 - (UITableView *)tableView{
     if (!_tableView ) {
         _tableView = [[UITableView alloc ] init];
+        [self.view addSubview:_tableView];
+        [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.right.left.equalTo(0);
+        }];
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.tableFooterView = [UIView new];
